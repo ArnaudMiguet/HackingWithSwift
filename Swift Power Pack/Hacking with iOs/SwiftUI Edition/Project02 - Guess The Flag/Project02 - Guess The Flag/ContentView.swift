35b72ac5d2bde5dee8f806d45c0e9a738a308fee
Arnaud Miguet <arnaud.miguet@icloud.com>
2020-11-29 13:22:11 +0100

    Completes project 02 of Swift UI Edition

---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project02 - Guess The Flag/Project02 - Guess The Flag/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Project02 - Guess The Flag
+//
+//  Created by Arnaud Miguet on 29.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                    Text("Score : \(score)")
+                }.foregroundColor(.white)
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        flagTapped(number)
+                    }) {
+                        Image(countries[number].lowercased()).clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+            }
+        }.alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
+                askQuestion()
+            })
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong, that was the flag of \(countries[number])"
+            score -= 1
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0..<3)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
