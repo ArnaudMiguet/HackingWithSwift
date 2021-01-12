d38dfbf368ae9abb6c865ee1eba499c70773aa0a
Arnaud Miguet <arnaud.miguet@icloud.com>
2021-01-12 17:55:33 +0100

    Adding projects 18

---
+++ Swift Power Pack/Hacking with iOs/UIKit Edition/Project18-Debugging/Project18-Debugging/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18-Debugging
+//
+//  Created by Arnaud Miguet on 12.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+            
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
