7f413911e1f22a0672cb271109d88dd2ed4017a8
Arnaud Miguet <arnaud.miguet@icloud.com>
2021-01-20 11:32:11 +0100

    Adding SwiftUI Project 05

---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project05-WordScramble/Project05-WordScramble/Project05_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project05_WordScrambleApp.swift
+//  Project05-WordScramble
+//
+//  Created by Arnaud Miguet on 20.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project05_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
