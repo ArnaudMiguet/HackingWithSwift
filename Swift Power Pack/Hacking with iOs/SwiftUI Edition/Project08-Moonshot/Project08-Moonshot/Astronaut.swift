50cdcce15b7a411e67c4458714e037683bf45874
Arnaud Miguet <arnaud.miguet@icloud.com>
2021-01-24 23:17:01 +0100

    Adding SwiftUI Project 08

---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project08-Moonshot/Project08-Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Project08-Moonshot
+//
+//  Created by Arnaud Miguet on 24.01.21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project08-Moonshot/Project08-Moonshot/Project08_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project08_MoonshotApp.swift
+//  Project08-Moonshot
+//
+//  Created by Arnaud Miguet on 24.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project08_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
