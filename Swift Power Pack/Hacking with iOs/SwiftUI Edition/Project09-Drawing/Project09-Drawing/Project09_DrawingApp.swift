f35236c9820851419ec97e868a96a3308f985268
Arnaud Miguet <arnaud.miguet@icloud.com>
2021-01-25 17:43:31 +0100

    Adding SwiftUI Project 09

---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project09-Drawing/Project09-Drawing/Project09_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project09_DrawingApp.swift
+//  Project09-Drawing
+//
+//  Created by Arnaud Miguet on 25.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project09_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
