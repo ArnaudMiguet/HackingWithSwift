4b720b46795196b25a327f858ffb082fbc57898d
Arnaud Miguet <arnaud.miguet@icloud.com>
2021-01-27 16:01:19 +0100

    Adding SwiftUI Project 16

---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project16-HotProspects/Project16-HotProspects/Project16_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project16_HotProspectsApp.swift
+//  Project16-HotProspects
+//
+//  Created by Arnaud Miguet on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project16_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
