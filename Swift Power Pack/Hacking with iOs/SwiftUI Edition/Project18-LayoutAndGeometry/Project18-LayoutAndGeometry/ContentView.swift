3ca5237105a79b3b5e83d49d739f91a8a23e8450
Arnaud Miguet <arnaud.miguet@icloud.com>
2021-01-28 15:07:25 +0100

    Adding SwiftUI Project 18

---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project18-LayoutAndGeometry/Project18-LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project18-LayoutAndGeometry
+//
+//  Created by Arnaud Miguet on 28.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Power Pack/Hacking with iOs/SwiftUI Edition/Project18-LayoutAndGeometry/Project18-LayoutAndGeometry/Project18_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project18_LayoutAndGeometryApp.swift
+//  Project18-LayoutAndGeometry
+//
+//  Created by Arnaud Miguet on 28.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project18_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
